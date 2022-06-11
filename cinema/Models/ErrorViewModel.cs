46d70c45fbf908dea561aed0fdd3230c6de21955
janl <janl@nux.cz>
2022-06-11 21:11:13 +0200

    sdd

--- cinema/Models/ErrorViewModel.cs
+++ cinema/Models/ErrorViewModel.cs
@@ -6,6 +6,6 @@ namespace cinema.Models
     {
         public string RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId =>    !string.IsNullOrEmpty(RequestId);
     }
 }
